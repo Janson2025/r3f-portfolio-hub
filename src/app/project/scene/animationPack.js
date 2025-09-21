7144c494ea916fc9e734d330de01583dc30c607f
jason2025 <callajames2025@gmail.com>
2025-09-21 11:43:36 -0600

    Reorders gesture files for consistency
    Reorders the list of gesture files in animationPack.js. This change aims to improve the consistency and maintainability of the file by grouping similar gestures.

--- src/app/project/scene/animationPack.js
+++ src/app/project/scene/animationPack.js
@@ -3,25 +3,25 @@ export const GESTURE_BASE = "/animations/Gestures-Pack-Basic/";
 
 // Comment any file that misbehaves (e.g., re-export later and re-enable)
 export const GESTURE_FILES = [
+  "happy-hand-gesture.fbx",
   "acknowledging.fbx",
   "angry-gesture.fbx",
+  "Swing-Dancing.fbx",
+  "thoughtful-head-shake.fbx",
   "annoyed-head-shake.fbx",
+  "Jumping.fbx",
   "being-cocky.fbx",
+  "Throw.fbx",
   "dismissing-gesture.fbx",
-  "happy-hand-gesture.fbx",
   "hard-head-nod.fbx",
+  "Falling-Idle.fbx",
   "head-nod-yes.fbx",
   "lengthy-head-nod.fbx",
   "look-away-gesture.fbx",
   "relieved-sigh.fbx",
   "sarcastic-head-nod.fbx",
   "shaking-head-no.fbx",
-  "thoughtful-head-shake.fbx",
-  "Jumping.fbx",
-  "Throw.fbx",
-  "Falling-Idle.fbx",
   "Robot-Hip-Hop-Dance.fbx",
-  "Swing-Dancing.fbx",
 ];
 
 export const DEFAULT_INITIAL_CLIP = "relieved-sigh";
